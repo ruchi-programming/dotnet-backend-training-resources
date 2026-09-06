4bce20e7f35e931c922994c66d5beaaefcf89775
Ruchi Swarup <152779443+ruchi-programming@users.noreply.github.com>
2026-09-06 22:13:01 +0530

    Initialize student registration API with basic setup

---
+++ 06-model-validation/student-registration-api/Program.cs
@@ -0,0 +1,10 @@
+WebApplicationBuilder builder =
+    WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+WebApplication app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
